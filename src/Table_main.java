d586a7ff7532bf17c4f46b123389fde4c48267c4
Soomin <ssoomin1@naver.com>
2020-11-02 19:53:13 +0900

    table main frame에서 panel로 수정

--- src/Table_main.java
+++ src/Table_main.java
@@ -1,113 +1,113 @@
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JLabel;
-import javax.swing.JTextPane;
-
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.swing.JTextField;
-//database ver.
-public class Table_main {
-	
-	/*frame을 extends로 바꾸고 패널 만들기
-	 * 변수 밖으로 빼기*/
-	
-	private JFrame frame;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Table_main extends JFrame {
+	private JPanel jp;
+	
+	//테이블 8개
+	private JButton table1;
+	private JButton table2;
+	private JButton table3;
+	private JButton table4;
+	private JButton table5;
+	private JButton table6;
+	private JButton table7;
+	private JButton table8;
+	//매출확인하는 버튼 
+	private JButton check_maechul;
+	//글자 
+	private JLabel price1;
+	
 	public static void main(String[] args) {
-			Table_main window = new Table_main();
-			window.frame.setVisible(true);
+		// TODO Auto-generated method stub
+		new Table_main();
 	}
-
-
 	public Table_main() {
 		initialize();
 	}
-
 	private void initialize() {
-		Connection conn=null;
-		PreparedStatement pstmt=null;
+		//데이터베이스 연동에 필요한 요소들
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 		ResultSet rs=null;
-		frame = new JFrame();
-		frame.setTitle("냠냠쩝쩝");
-		frame.setBounds(100, 100, 761, 520);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		frame.setLocationRelativeTo(null);	//창이 가운데에서 실행
-		
+		setTitle("냠냠쩝쩝");
+		setBounds(100,100,761,520);
+		setLocationRelativeTo(null);	//창이 가운데에서 실행
 		try {
-			//DB연동
 			Class.forName("com.mysql.jdbc.Driver");
 			String url="jdbc:mysql://localhost/yumyum1";
-			//mysql을 아예 내가 올려주면 은별이가 pull받으면 될 것 같음.
-			conn=DriverManager.getConnection(url,"gogi1","2203");
+			//mysql 접근 database,테이블 이름,비밀번호
+			conn=DriverManager.getConnection(url,"gogi1","2209");
 			System.out.println("연결 성공");
+			//gid 몇번째 고기인지 선택 => 메뉴에서 버튼을 선택하면 그 아이디가 넘어간다.
 			int num=1;
 			String sql="select * from meatmenu where gid = "+num+";";
+			//결과값을 받기위해서 
 			pstmt=conn.prepareStatement(sql);
 			//이 문장을 실행함으로써 결과값을 rs에 넣어준다.
 			rs=pstmt.executeQuery(sql);
 			while (rs.next()) {
 				String meat_name=rs.getString("meat_name");
 				int price=rs.getInt("meat_price");
-				
+				jp=new JPanel();
+				jp.setLayout(null);
 				System.out.println("고기 종류: "+meat_name+" / 가격: "+price);
+				price1=new JLabel();
+				price1.setBounds(10, 10, 147, 30);
+				price1.setText("가격 : " + Integer.toString(price));
 				
-				JLabel label = new JLabel();
-//				label.setAlignmentX(0);
-//				label.setAlignmentY(0);
-				label.setBounds(10, 10, 147, 30);
-
-				label.setText("가격 : " + Integer.toString(price));
-				
-				JButton button1 = new JButton("테이블 1");
-				button1.addActionListener(new ActionListener() {
+				table1 = new JButton("테이블 1");
+				table1.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						//버튼을 누르면 다음 패널로 넘어가욤
 						
 					}
 				});
-				button1.setBounds(29, 47, 147, 127);
-				frame.getContentPane().add(button1);
-				button1.add(label);
+				table1.setBounds(29, 47, 147, 127);
+				table1.add(price1);
 				
-				JButton button2 = new JButton("테이블 2");
-				button2.setBounds(215, 47, 147, 127);
-				frame.getContentPane().add(button2);
+				table2 = new JButton("테이블 2");
+				table2.setBounds(215, 47, 147, 127);
 		
-				JButton button3 = new JButton("테이블 3");
-				button3.setBounds(397, 47, 147, 127);
-				frame.getContentPane().add(button3);
+			    table3 = new JButton("테이블 3");
+				table3.setBounds(397, 47, 147, 127);
 		
-				JButton button4 = new JButton("테이블 4");
-				button4.setBounds(582, 47, 147, 127);
-				frame.getContentPane().add(button4);
+				table4 = new JButton("테이블 4");
+				table4.setBounds(582, 47, 147, 127);
 		
-				JButton button5 = new JButton("테이블 5");
-				button5.setBounds(29, 247, 147, 127);
-				frame.getContentPane().add(button5);
+				table5 = new JButton("테이블 5");
+				table5.setBounds(29, 247, 147, 127);
 		
-				JButton button6 = new JButton("테이블 6");
-				button6.setBounds(215, 247, 147, 127);
-				frame.getContentPane().add(button6);
+				table6 = new JButton("테이블 6");
+				table6.setBounds(215, 247, 147, 127);
+
+				table7 = new JButton("테이블 7");
+				table7.setBounds(397, 247, 147, 127);
 		
-				JButton button7 = new JButton("테이블 7");
-				button7.setBounds(397, 247, 147, 127);
-				frame.getContentPane().add(button7);
-		
-				JButton button8 = new JButton("테이블 8");
-				button8.setBounds(582, 247, 147, 127);
-				frame.getContentPane().add(button8);
-		
-				JButton checkbtn = new JButton("매출확인");
-				checkbtn.setBounds(609, 420, 120, 27);
-				frame.getContentPane().add(checkbtn);
-				}
+				table8 = new JButton("테이블 8");
+				table8.setBounds(582, 247, 147, 127);
+
+				check_maechul = new JButton("매출확인");
+				check_maechul.setBounds(609, 420, 120, 27);
+				
+				jp.add(table1); 
+				jp.add(table2); jp.add(table3); jp.add(table4);
+				jp.add(table5); jp.add(table6); jp.add(table7); jp.add(table8);
+				jp.add(check_maechul);
+				add(jp);
+				
+				setVisible(true);
+			}
 			conn.close();
 		}catch(ClassNotFoundException e) {
 			System.out.println("드라이버 로딩 실패");
@@ -119,10 +119,11 @@ public class Table_main {
 				if(conn!=null && !conn.isClosed()) {
 					conn.close();
 				}
-				if(pstmt!=null) try {pstmt.close();} catch(SQLException se) {}
+				if(pstmt!=null) try {pstmt.close();} catch(SQLException se) { }
 			}catch(SQLException se) {
 				se.printStackTrace();
 			}
 		}
 	}
+
 }
