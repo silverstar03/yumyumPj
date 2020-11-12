3c66caf8fa4b1590d1388c14c936b45a7b31077a
eun byeol kang <eunbyoul0421@naver.com>
2020-11-13 04:04:07 +0900

    cardPaying 오류 수정

--- src/yumyum/OrderMenu.java
+++ src/yumyum/OrderMenu.java
@@ -75,7 +75,7 @@ public class OrderMenu extends JFrame {
 	
 		
 	//생성자 메서드
-	public OrderMenu(String table_num, Table_main t_main, Pay pay) {
+	public OrderMenu(String table_num, Table_main t_main) {
 		
 		this.getContentPane().repaint();	//컴포넌트 재배치(새로고침 개념)
 		setTitle("주문 등록 화면");
@@ -92,10 +92,10 @@ public class OrderMenu extends JFrame {
 		title_label.setBounds(448, 2, 187, 60);
 		add(title_label);
 				
-		menu(table_num, t_main, pay);
+		menu(table_num, t_main);
 	}
 	
-	public void menu(String table_num, Table_main t_main, Pay pay) {	//메뉴 버튼들 생성
+	public void menu(String table_num, Table_main t_main) {	//메뉴 버튼들 생성
 		
 		menu_panel = new JPanel();
 		menu_panel.setBounds(490, 72, 600, 84);
@@ -223,6 +223,7 @@ public class OrderMenu extends JFrame {
 			public void mouseClicked(MouseEvent e) {
 				if(e.getClickCount() == 1) {
 					setVisible(false);
+					pay = new Pay(t_main, table_num);
 					pay.setVisible(true);
 				}
 			}
